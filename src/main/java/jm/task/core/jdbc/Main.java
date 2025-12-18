bb2fd611432c1b3a6a10016c033e4db3e2e0a61a
Abelyh <Al.belyh86@mail.ru>
2025-12-18 20:27:55 +0300

    Исправление ошибок

--- src/main/java/jm/task/core/jdbc/Main.java
+++ src/main/java/jm/task/core/jdbc/Main.java
@@ -1,16 +1,15 @@
 package jm.task.core.jdbc;
 
-
 import jm.task.core.jdbc.model.User;
+import jm.task.core.jdbc.service.UserService;
 import jm.task.core.jdbc.service.UserServiceImpl;
 
-import java.util.ArrayList;
 
 public class Main {
     public static void main(String[] args) {
         // реализуйте алгоритм здесь
 
-        UserServiceImpl userService = new UserServiceImpl();
+        UserService userService = new UserServiceImpl();
 
         userService.createUsersTable();
 
