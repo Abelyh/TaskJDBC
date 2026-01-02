c169d747b45771583d1e8777c7d47db521043965
Abelyh <Al.belyh86@mail.ru>
2026-01-02 20:33:43 +0300

    solving the task

--- 2.2.1. spring_hibernate/src/main/java/hiber/dao/UserDaoImp.java
+++ 2.2.1. spring_hibernate/src/main/java/hiber/dao/UserDaoImp.java
@@ -1,7 +1,11 @@
 package hiber.dao;
 
+import hiber.model.Car;
 import hiber.model.User;
+import org.hibernate.Session;
 import org.hibernate.SessionFactory;
+import org.hibernate.query.NativeQuery;
+import org.hibernate.query.Query;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Repository;
 
@@ -11,19 +15,40 @@ import java.util.List;
 @Repository
 public class UserDaoImp implements UserDao {
 
-   @Autowired
-   private SessionFactory sessionFactory;
+    @Autowired
+    private SessionFactory sessionFactory;
 
-   @Override
-   public void add(User user) {
-      sessionFactory.getCurrentSession().save(user);
-   }
 
-   @Override
-   @SuppressWarnings("unchecked")
-   public List<User> listUsers() {
-      TypedQuery<User> query=sessionFactory.getCurrentSession().createQuery("from User");
-      return query.getResultList();
-   }
+    private long existByEmail(User user) {
+        Session currentSession = sessionFactory.getCurrentSession();
+        Query<Long> query = currentSession.createQuery("SELECT COUNT(u) FROM User u WHERE u.email = :email", Long.class);
+        query.setParameter("email", user.getEmail());
+        return query.uniqueResult();
+    }
+
+    @Override
+    public void add(User user) {
+        if (existByEmail(user) == 0) {
+            sessionFactory.getCurrentSession().save(user);
+        } else {
+            System.out.println("A user with this email address already exists.");
+        }
+    }
+
+    @Override
+    @SuppressWarnings("unchecked")
+    public List<User> listUsers() {
+        TypedQuery<User> query = sessionFactory.getCurrentSession().createQuery("from User");
+        return query.getResultList();
+    }
+
+    @Override
+    public List<User> getUser(Car car) {
+        Session currentSession = sessionFactory.getCurrentSession();
+        TypedQuery<User> query = currentSession.createQuery("FROM User u WHERE u.car.model = :model AND u.car.series = :series", User.class);
+        query.setParameter("model", car.getModel());
+        query.setParameter("series", car.getSeries());
+        return query.getResultList();
+    }
 
 }
